72ae34b68a31eb3faaa9e5a3952694c53291ac54
laacevedovalle-jpg <la.acevedovalle@gmail.com>
2025-08-08 11:42:20 -0600

    Add files via upload

---
+++ Semana1/Ejercicio1_Ago8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+float numeros[5];
+numeros[0]=.5;
+numeros[1]=1.5;
+numeros[2]=2.5;
+numeros[3]=3.5;
+numeros[4]=4.5;
+int i;
+
+printf("%f \n", numeros);
+}

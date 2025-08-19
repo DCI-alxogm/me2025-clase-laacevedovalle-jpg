d067e83f4a7a48c75849ed7f94c8493cd2d0220a
laacevedovalle-jpg <la.acevedovalle@gmail.com>
2025-08-19 13:57:29 -0400

    Add files via upload

---
+++ Semana 3/Ejercicio1_Ago19.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float x;
+  float es;
+  int n;
+  float ea;
+
+  printf("Introduzca X:");
+  scanf("%f", &x);
+  printf("\nAhora introduzca el error:");
+  scanf("%f", &es);
+
+  
+  
+  
+  
+  
+  
+  
+  
+  printf("\nX es igual a %f", x);
+}
